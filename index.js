11dea17450e43713e7ee81887b0303613a820a67
Stacie Rogers <stacierogers92@icloud.com>
2023-01-09 20:05:01 -0600

    Create a CodeGrade submission

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Code your solution in this file!
+const returnFirstTwoDrivers = function (drivers) {
+  return drivers.slice(0, 2);
+}
+const returnLastTwoDrivers = function (drivers) {
+  return drivers.slice(-2)
+}
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
+const createFareMultiplier = function (multiplyValue) {
+  return function (value) {
+    return value * multiplyValue
+  }
+}
+const fareDoubler = createFareMultiplier(2)
+const fareTripler = createFareMultiplier(3)
+const selectDifferentDrivers = function (drivers, driversToReturn) {
+  return driversToReturn(drivers)
+}
